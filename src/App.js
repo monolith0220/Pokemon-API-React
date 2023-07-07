d9e71ba0a48229bcfed8dbacdbdc8cd738a47016
monolith0220 <funky.atmos.worlddd@gmail.com>
2023-07-07 14:57:24 +0900

    Loading Settings

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchPokemonData = async ()=> {
       let res = await getAllPokemon(initialURL)
       console.log(res)
+      setLoading(false)
     }
     fetchPokemonData()
   }, [])
 
   return (
     <div className="App">
+      {loading ? (
+        <p>ローディング中</p> ): (
+          <p>ポケモンデータ取得できました</p>
+        )}
     </div>
-  );
-}
-
+  )
+        }
 export default App;
